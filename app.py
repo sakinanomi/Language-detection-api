7693d658f55a4f868ac8a784f0ededd75b3b9c58
sakinanomi <56465213+sakinanomi@users.noreply.github.com>
2021-12-20 10:02:53 +0530

    comments

--- app.py
+++ app.py
@@ -25,15 +25,16 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def check():
-    data = json.loads(request.data)
-    text = data.get("text",None)
+    data = json.loads(request.data) #json.loads parses the json object into python dict . request.data requests for data
+    text = data.get("text",None) # get method returns the value of the item with specified key
+
     if text is None:
         return jsonify({"message":"text not found"})
     else:
         t=data['text']
-        t=preprocess(t)
-        t=[t]
-        ans=pred(t)
+        t=preprocess(t) #making int lower
+        t=[t] #converting into list
+        ans=pred(t) #predict the language
         return jsonify({"predicted ":ans})
 
 
@@ -45,3 +46,9 @@ def index():
 if __name__ == '__main__':
 
     app.run(debug=True)
+
+
+
+
+## deployed at heroku
+#  https://la-identification.herokuapp.com/predict
